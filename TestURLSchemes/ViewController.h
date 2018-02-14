b8090d592e2e64545feb8d2493b15c1b1ff41fee
whoami <yar.spirin@gmail.com>
2018-02-14 14:46:29 +0300

    initial commit

---
+++ TestURLSchemes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestURLSchemes
+//
+//  Created by whoami on 2/9/18.
+//  Copyright © 2018 Mountain Viewer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestURLSchemes/ViewController.m
@@ -0,0 +1,65 @@
+//
+//  ViewController.m
+//  TestURLSchemes
+//
+//  Created by whoami on 2/9/18.
+//  Copyright © 2018 Mountain Viewer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (IBAction)facebookButtonTapped:(id)sender {
+    UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"Open Facebook" message:@"You're going to open facebook profile page." preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"Confirm" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        NSURL *url = [NSURL URLWithString:@"fb://profile?id=1494886206"];
+        if ([[UIApplication sharedApplication] canOpenURL:url]) {
+            [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+        }
+    }];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+    
+    [alertVC addAction:confirmAction];
+    [alertVC addAction:cancelAction];
+    
+    [self presentViewController:alertVC animated:YES completion:nil];
+}
+
+- (IBAction)linkedinButtonTapped:(id)sender {
+    UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"Open Facebook" message:@"You're going to open facebook profile page." preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"Confirm" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        NSURL *url = [NSURL URLWithString:@"voyager://in/nikitavspirin"];
+        if ([[UIApplication sharedApplication] canOpenURL:url]) {
+            [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+        }
+    }];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+    
+    [alertVC addAction:confirmAction];
+    [alertVC addAction:cancelAction];
+    
+    [self presentViewController:alertVC animated:YES completion:nil];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
